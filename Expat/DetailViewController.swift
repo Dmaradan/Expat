e2dedbd44b75a790e6dab13238c2fbd66a522033
Diego Martin <diegomartin1313@gmail.com>
2015-12-16 16:21:39 -0800

    refactor: Refactor code for using API response data

--- Expat/DetailViewController.swift
+++ Expat/DetailViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    // MARK: Config
+                                    // MARK: Config
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
     var city: City?
 	   var averagePrice: String?
 	
-    // MARK: Outlets
+                                     // MARK: Outlets
     
     @IBOutlet weak var name: UILabel!
     
@@ -36,9 +36,9 @@ class DetailViewController: UIViewController {
         image.image = city?.image
         shortDescription.text = city?.description
         
-        // MARK: API Call
+                                      // MARK: API Call
         
-        /* Build the URL */
+								/* Build the URL */
         
         var string1 = appDelegate.baseURLString
         
@@ -54,13 +54,13 @@ class DetailViewController: UIViewController {
         let url = NSURL(string: string1 + string2)
         
         
-        /* Build the Request */
+								/* Build the Request */
         
         let request = NSMutableURLRequest(URL: url!)
         request.HTTPMethod = "GET"
         
         
-        /* Set the Session */
+								/* Set the Session */
         
         let session = NSURLSession.sharedSession()
         
@@ -75,7 +75,7 @@ class DetailViewController: UIViewController {
             }
             
             
-            /* Parse the Data */
+         /* Parse the Data */
             
             let parsedResult: AnyObject!
             do {
@@ -88,30 +88,61 @@ class DetailViewController: UIViewController {
             }
 									
 	
-                    /* Use the Data */
-									
-									let recent_month = (parsedResult["response"]!!["most_recent_month"]) as! String
+                                     // MARK: Use the Data 
 									
 									/* Go to the array index that corresponds to '2 bedroom rent monthly' */
-									let two_bed_rent_monthly = parsedResult["response"]!!["metadata"]!![2]
+									
+									/* GUARD: Is the 'response' key in parsedResult? */
+         guard let theResponse = parsedResult["response"] as? NSDictionary else {
+										    print("The key 'response' was not found in \(parsedResult)")
+										    return
+									}
+									
+									/* GUARD: Is the 'most_recent_month' key in theResponse? */
+									guard let recent_month = theResponse["most_recent_month"] as? String else {
+										    print("The key 'most_recent_month' was not found in \(theResponse)")
+										    return
+									}
+									
+									/* GUARD: Is the 'metadata' key in theResponse? */
+									guard let metadata = theResponse["metadata"] as? NSArray else {
+										    print("the key 'metadata' was not found in \(theResponse)")
+										    return
+									}
+									
+									
+									let two_bed_rent_monthly = metadata[2]
 									
 									
 									
 									/* Go to the latest month and average price */
-									print(two_bed_rent_monthly["data"]!!["\(recent_month)"]!!["avg_price"])
-									let myString = (two_bed_rent_monthly["data"]!!["\(recent_month)"]!!["avg_price"]) as! String
 									
+									/* GUARD: is the 'data' key in two_bed_rent_monthly? */
+									guard let theData = two_bed_rent_monthly["data"] as? NSDictionary else {
+										    print("The key 'data' was not found in \(two_bed_rent_monthly)")
+										    return
+									}
+									
+									/* GUARD: is the most recent month a key in theData? */
+									guard let monthlyData = theData["\(recent_month)"] as? NSDictionary else {
+										    print("The key \(recent_month) was not found in \(theData)")
+										    return
+									}
+									
+									/* GUARD: is the key 'avg_price' in monthlyData? */
+									guard let theAveragePrice = monthlyData["avg_price"] as? String else {
+										    print("The key 'avg_price' was not found in \(monthlyData)")
+										    return
+									}
+
 									/* Set the average price */
-									let averagePrice = NSNumberFormatter().numberFromString(myString)?.doubleValue
 									
-									self.averagePriceLabel.text = "Average Monthly Rent for Two-Bedroom Property: " + String(averagePrice)
+									self.averagePriceLabel.text = "Average Monthly Rent for Two-Bedroom Property: " + theAveragePrice
+									
+									let averagePrice = NSNumberFormatter().numberFromString(theAveragePrice)?.doubleValue
 									
 									self.appDelegate.averagePrice = averagePrice
 									
-
-									
-									/* Try to find my soul again */
-
         }
         
         task.resume()
